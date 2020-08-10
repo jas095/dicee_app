a6291abc1b955b21da52c8f89f811ad512210f35
Jose Angel <jas@deb>
2020-08-09 23:44:26 -0400

    End of 'Las Vegas' dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: _getDiceNumber,
+              child: Image.asset(
+                'images/dint$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: _getDiceNumber,
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _getDiceNumber() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
